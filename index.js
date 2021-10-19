02c2f6d90fdcf2a13271c75298bc5010b23302c0
cocatrip <cocainetrip@protonmail.com>
2021-10-19 21:55:25 +0700

    sync

--- index.js
+++ index.js
@@ -1,238 +1,143 @@
-$( window ).resize( function() {
-  centerSplash();
+var ITEManimate = ({
+    start: 0,
+    bezier: function(p0, p1, p2, p3) {
+        return ITEManimate.polyBez([p0, p1], [p2, p3]);
+
+    },
+    polyBez: function(p1, p2) {
+        var A = [null, null],
+            B = [null, null],
+            C = [null, null],
+            bezCoOrd = function(t, ax) {
+                C[ax] = 3 * p1[ax], B[ax] = 3 * (p2[ax] - p1[ax]) - C[ax], A[ax] = 1 - C[ax] - B[ax];
+                return t * (C[ax] + t * (B[ax] + t * A[ax]));
+            },
+            xDeriv = function(t) {
+                return C[0] + t * (2 * B[0] + 3 * A[0] * t);
+            },
+            xForT = function(t) {
+                var x = t,
+                    i = 0,
+                    z;
+                while (++i < 14) {
+                    z = bezCoOrd(x, 0) - t;
+                    if (Math.abs(z) < 1e-3) break;
+                    x -= z / xDeriv(x);
+                }
+                return x;
+            };
+        return function(t) {
+            return bezCoOrd(xForT(t), 1);
+        }
+    }
 });
 
-!function($){
 
-    "use strict";
+//CUSTOM JS CODE
+(function($) {
+    'use strict';
 
-    var Typed = function(el, options){
+    //VIEWPORT
+    var w = $(window);
 
-        // chosen element to manipulate text
-        this.el = $(el);
-        // options
-        this.options = $.extend({}, $.fn.typed.defaults, options);
+    //ANIMATION
+    var animationTrigger = $('.trigger');
+    var sceneContainer = $('.slide-wrapper');
+    var smallCircles = $('.small-circle');
+    var portfolioContainer = $('.portfolio-wrapper');
 
-        // text content of element
-        this.text = this.el.text();
+    var main = {
 
-        // typing speed
-        this.typeSpeed = this.options.typeSpeed;
+        init: function() {
+            var self = this;
+            //GSAP ANIMATE
+            main.animate();
+        },
 
-        // amount of time to wait before backspacing
-        this.backDelay = this.options.backDelay;
+        //GSAP ANIMATION
+        animate: function() {
 
-        // input strings of text
-        this.strings = this.options.strings;
+            //OPEN
+            function openAnimation() {
+                TweenMax.to(sceneContainer, 0.8, {
+                    height: "100%",
+                    ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
+                    top: "0%",
+                    ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
+                    width: "100%",
+                    ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
+                    left: "0%",
+                    ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
 
-        // character number position of current string
-        this.strPos = 0;
+                    onComplete: function() {
+                      console.log(sceneContainer);
+                        TweenMax.to(portfolioContainer, 1.8, {
+                            width: "100%",
+                            ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
+                            top: "30%",
+                            ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815)
+                        })
+                        TweenMax.to(sceneContainer, 0.8, {
+                            top: "-70%",
+                            ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
+                        })
+                    }
+                });
 
-        // current array position
-        this.arrayPos = 0;
+                TweenMax.to(smallCircles, 0.4, {
+                    scale: "0",
+                    ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
 
-        // current string based on current values[] array position
-        this.string = this.strings[this.arrayPos];
+                });
 
-        // number to stop backspacing on.
-        // default 0, can change depending on how many chars
-        // you want to remove at the time
-        this.stopNum = 0;
-
-        // Looping logic
-        this.loop = this.options.loop;
-        this.loopCount = this.options.loopCount;
-        this.curLoop = 1;
-        if (this.loop === false){
-            // number in which to stop going through array
-            // set to strings[] array (length - 1) to stop deleting after last string is typed
-            this.stopArray = this.strings.length-1;
-        }
-        else{
-            this.stopArray = this.strings.length;
-        }
-
-        // All systems go!
-        this.init();
-        this.build();
-    }
-
-        Typed.prototype =  {
-
-            constructor: Typed
-
-            , init: function(){
-                // begin the loop w/ first current string (global self.string)
-                // current string will be passed as an argument each time after this
-                this.typewrite(this.string, this.strPos);
             }
 
-            , build: function(){
-                this.el.after("<span id=\"typed-cursor\">|</span>");
-            }
+            //CLOSE
+            function closeAnimation() {
+                TweenMax.to(portfolioContainer, 1.2, {
+                    top: "100%",
+                    ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
+                    width: "100%",
+                    ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
 
-            // pass current string state to each function
-            , typewrite: function(curString, curStrPos){
+                    onComplete: function() {
+                        TweenMax.to(sceneContainer, 0.8, {
+                            height: "90%",
+                            ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
+                            top: "5%",
+                            ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
+                            width: "90%",
+                            ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
+                            left: "5%",
+                            ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
+                            onComplete: function() {
+                                TweenMax.to(smallCircles, 0.4, {
+                                    scale: "1",
+                                    ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
 
-                // varying values for setTimeout during typing
-                // can't be global since number changes each time loop is executed
-                var humanize = Math.round(Math.random() * (100 - 30)) + this.typeSpeed;
-                var self = this;
+                                });
 
-                // ------------- optional ------------- //
-                // backpaces a certain string faster
-                // ------------------------------------ //
-                // if (self.arrayPos == 1){
-                //  self.backDelay = 50;
-                // }
-                // else{ self.backDelay = 500; }
-
-                // containg entire typing function in a timeout
-                setTimeout(function() {
-
-                    // make sure array position is less than array length
-                    if (self.arrayPos < self.strings.length){
-
-                        // start typing each new char into existing string
-                        // curString is function arg
-                        self.el.text(self.text + curString.substr(0, curStrPos));
-
-                        // check if current character number is the string's length
-                        // and if the current array position is less than the stopping point
-                        // if so, backspace after backDelay setting
-                        if (curStrPos > curString.length && self.arrayPos < self.stopArray){
-                            clearTimeout(clear);
-                            var clear = setTimeout(function(){
-                                self.backspace(curString, curStrPos);
-                            }, self.backDelay);
-                        }
-
-                        // else, keep typing
-                        else{
-                            // add characters one by one
-                            curStrPos++;
-                            // loop the function
-                            self.typewrite(curString, curStrPos);
-                            // if the array position is at the stopping position
-                            // finish code, on to next task
-                            if (self.loop === false){
-                                if (self.arrayPos === self.stopArray && curStrPos === curString.length){
-                                    // animation that occurs on the last typed string
-                                    // fires callback function
-                                    var clear = self.options.callback();
-                                    clearTimeout(clear);
-                                }
                             }
-                        }
-                    }
-                    // if the array position is greater than array length
-                    // and looping is active, reset array pos and start over.
-                    else if (self.loop === true && self.loopCount === false){
-                        self.arrayPos = 0;
-                        self.init();
-                    }
-                        else if(self.loopCount !== false && self.curLoop < self.loopCount){
-                            self.arrayPos = 0;
-                            self.curLoop = self.curLoop+1;
-                            self.init();
-                        }
 
-                // humanized value for typing
-                }, humanize);
+                        });
+                    }
+
+                })
 
             }
 
-            , backspace: function(curString, curStrPos){
-
-                // varying values for setTimeout during typing
-                // can't be global since number changes each time loop is executed
-                var humanize = Math.round(Math.random() * (100 - 30)) + this.typeSpeed;
-                var self = this;
-
-                setTimeout(function() {
-
-                    // ----- this part is optional ----- //
-                    // check string array position
-                    // on the first string, only delete one word
-                    // the stopNum actually represents the amount of chars to
-                    // keep in the current string. In my case it's 14.
-                     if (self.arrayPos == 1, 2, 3, 4){
-                        self.stopNum = 0;
-                     }
-                    //every other time, delete the whole typed string
-                     //else{
-                        //self.stopNum = 0;
-                     //}
-
-                    // ----- continue important stuff ----- //
-                    // replace text with current text + typed characters
-                    self.el.text(self.text + curString.substr(0, curStrPos));
-
-                    // if the number (id of character in current string) is
-                    // less than the stop number, keep going
-                    if (curStrPos > self.stopNum){
-                        // subtract characters one by one
-                        curStrPos--;
-                        // loop the function
-                        self.backspace(curString, curStrPos);
-                    }
-                    // if the stop number has been reached, increase
-                    // array position to next string
-                    else if (curStrPos <= self.stopNum){
-                        clearTimeout(clear);
-                        var clear = self.arrayPos = self.arrayPos+1;
-                        // must pass new array position in this instance
-                        // instead of using global arrayPos
-                        self.typewrite(self.strings[self.arrayPos], curStrPos);
-                    }
-
-                // humanized value for typing
-                }, humanize);
-
-            }
-
+            animationTrigger.click(function() {
+                if ($(this).attr('data-toggle') == 'closed') {
+                    $(this).attr('data-toggle', 'opened');
+                    openAnimation();
+                } else if ($(this).attr('data-toggle', 'opened')) {
+                    $(this).attr('data-toggle', 'closed');
+                    closeAnimation();
+                }
+            });
         }
+    };
 
-    $.fn.typed = function (option) {
-        return this.each(function () {
-          var $this = $(this)
-            , data = $this.data('typed')
-            , options = typeof option == 'object' && option
-          if (!data) $this.data('typed', (data = new Typed(this, options)))
-          if (typeof option == 'string') data[option]()
-        });
-    }
-
-    $.fn.typed.defaults = {
-        strings: ["Hello, hola, hi! ", "Welcome to my website ", "Go on, scroll down", ":)"],
-        // typing and backspacing speed
-        typeSpeed: 50, // speed decreases as number increased
-        // time before backspacing
-        backDelay: 100,
-        // loop
-        loop: true,
-        // false = infinite
-        loopCount: false,
-        // ending callback function
-        callback: function(){ null }
-    }
-
-
-}(window.jQuery);
-
-
-$(function(){
-
-        $("#typed").typed({
-            strings: ["web developer","geek","human being"], //Strings to display when typing
-            typeSpeed: 40,
-            backDelay: 600,
-            loop: true,
-            // defaults to false for infinite loop
-            loopCount: false,
-            callback: function(){ foo(); }
-        });
-
-        function foo(){ console.log("Callback"); }
-
-    });
+    $(window).resize(function() {});
+    return main.init();
+}($));
