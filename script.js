9b3036c8d896c5c473f4f42cf5a700e8d507c4a1
cocatrip <cocainetrip@protonmail.com>
2021-11-15 11:49:27 +0700

    sync

--- script.js
+++ script.js
@@ -1,150 +1,156 @@
-/**
- * ITEManimate object is used to animate ease with bezier functions
- * example: TweenMax.to($('selector'), 1.5, {left:"80%", ease: ITEManimate.bezier(0.04,0.86,0.8,1)});
- *
- * Used: https://github.com/rdallasgray/bez
-   Forked from: https://codepen.io/karlovidek/pen/qOxYjp
- */
-var ITEManimate = ({
-    start: 0,
-    bezier: function(p0, p1, p2, p3) {
-        return ITEManimate.polyBez([p0, p1], [p2, p3]);
+// Slider(all Slides in a container)
+const slider = document.querySelector(".slider")
+// All trails 
+const trail = document.querySelector(".trail").querySelectorAll("div")
 
-    },
-    polyBez: function(p1, p2) {
-        var A = [null, null],
-            B = [null, null],
-            C = [null, null],
-            bezCoOrd = function(t, ax) {
-                C[ax] = 3 * p1[ax], B[ax] = 3 * (p2[ax] - p1[ax]) - C[ax], A[ax] = 1 - C[ax] - B[ax];
-                return t * (C[ax] + t * (B[ax] + t * A[ax]));
-            },
-            xDeriv = function(t) {
-                return C[0] + t * (2 * B[0] + 3 * A[0] * t);
-            },
-            xForT = function(t) {
-                var x = t,
-                    i = 0,
-                    z;
-                while (++i < 14) {
-                    z = bezCoOrd(x, 0) - t;
-                    if (Math.abs(z) < 1e-3) break;
-                    x -= z / xDeriv(x);
-                }
-                return x;
-            };
-        return function(t) {
-            return bezCoOrd(xForT(t), 1);
-        }
+// Transform value
+let value = 0
+// trail index number
+let trailValue = 0
+// interval (Duration)
+let interval = 4000
+
+// Function to slide forward
+const slide = (condition) => {
+    // CLear interval
+    clearInterval(start)
+    // update value and trailValue
+    condition === "increase" ? initiateINC() : initiateDEC()
+    // move slide
+    move(value, trailValue)
+    // Restart Animation
+    animate()
+    // start interal for slides back 
+    start = setInterval(() => slide("increase"), interval);
+}
+
+// function for increase(forward, next) configuration
+const initiateINC = () => {
+    // Remove active from all trails
+    trail.forEach(cur => cur.classList.remove("active"))
+    // increase transform value
+    value === 80 ? value = 0 : value += 20
+    // update trailValue based on value
+    trailUpdate()
+}
+
+// function for decrease(backward, previous) configuration
+const initiateDEC = () => {
+     // Remove active from all trails
+    trail.forEach(cur => cur.classList.remove("active"))
+    // decrease transform value
+    value === 0 ? value = 80 : value -= 20
+     // update trailValue based on value
+    trailUpdate()
+}
+
+// function to transform slide 
+const move = (S, T) => {
+    // transform slider
+    slider.style.transform = `translateX(-${S}%)`
+    //add active class to the current trail
+    trail[T].classList.add("active")
+}
+
+const tl = gsap.timeline({defaults: {duration: 0.6, ease: "power2.inOut"}})
+tl.from(".bg", {x: "-100%", opacity: 0})
+  .from("p", {opacity: 0}, "-=0.3")
+  .from("h1", {opacity: 0, y: "30px"}, "-=0.3")
+  .from("button", {opacity: 0, y: "-40px"}, "-=0.8")
+
+// function to restart animation
+const animate = () => tl.restart()
+
+// function to update trailValue based on slide value
+const trailUpdate = () => {
+    if (value === 0) {
+        trailValue = 0
+    } else if (value === 20) {
+        trailValue = 1
+    } else if (value === 40) {
+        trailValue = 2
+    } else if (value === 60) {
+        trailValue = 3
+    } else {
+        trailValue = 4
     }
-});
+}   
 
+// Start interval for slides
+let start = setInterval(() => slide("increase"), interval)
 
-//CUSTOM JS CODE
-(function($) {
-    'use strict';
+// Next  and  Previous button function (SVG icon with different classes)
+document.querySelectorAll("svg").forEach(cur => {
+    // Assign function based on the class Name("next" and "prev")
+    cur.addEventListener("click", () => cur.classList.contains("next") ? slide("increase") : slide("decrease"))
+})
 
-    //VIEWPORT
-    var w = $(window);
+// function to slide when trail is clicked
+const clickCheck = (e) => {
+    // CLear interval
+    clearInterval(start)
+    // remove active class from all trails
+    trail.forEach(cur => cur.classList.remove("active"))
+    // Get selected trail
+    const check = e.target
+    // add active class
+    check.classList.add("active")
 
-    //ANIMATION
-    var animationTrigger = $('.trigger');
-    var sceneContainer = $('.slide-wrapper');
-    var smallCircles = $('.small-circle');
-    var portfolioContainer = $('.portfolio-wrapper');
+    // Update slide value based on the selected trail
+    if(check.classList.contains("box1")) {
+        value = 0
+    } else if (check.classList.contains("box2")) {
+        value = 20
+    } else if (check.classList.contains("box3")) {
+        value = 40
+    } else if (check.classList.contains("box4")) {
+        value = 60
+    } else {
+        value = 80
+    }
+    // update trail based on value
+    trailUpdate()
+    // transfrom slide
+    move(value, trailValue)
+    // start animation
+    animate()
+    // start interval
+    start = setInterval(() => slide("increase"), interval)
+}
 
-    var main = {
+// Add function to all trails
+trail.forEach(cur => cur.addEventListener("click", (ev) => clickCheck(ev)))
 
-        init: function() {
-            var self = this;
-            //GSAP ANIMATE
-            main.animate();
-        },
+// Mobile touch Slide Section
+const touchSlide = (() => {
+    let start, move, change, sliderWidth
 
-        //GSAP ANIMATION
-        animate: function() {
+    // Do this on initial touch on screen
+    slider.addEventListener("touchstart", (e) => {
+        // get the touche position of X on the screen
+        start = e.touches[0].clientX
+        // (each slide with) the width of the slider container divided by the number of slides
+        sliderWidth = slider.clientWidth/trail.length
+    })
+    
+    // Do this on touchDrag on screen
+    slider.addEventListener("touchmove", (e) => {
+        // prevent default function
+        e.preventDefault()
+        // get the touche position of X on the screen when dragging stops
+        move = e.touches[0].clientX
+        // Subtract initial position from end position and save to change variabla
+        change = start - move
+    })
 
-            //OPEN
-            function openAnimation() {
-                TweenMax.to(sceneContainer, 0.8, {
-                    height: "100%",
-                    ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
-                    top: "0%",
-                    ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
-                    width: "100%",
-                    ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
-                    left: "0%",
-                    ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
-
-                    onComplete: function() {
-                      console.log(sceneContainer);
-                        TweenMax.to(portfolioContainer, 1.8, {
-                            width: "100%",
-                            ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
-                            top: "30%",
-                            ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815)
-                        })
-                        TweenMax.to(sceneContainer, 0.8, {
-                            top: "-70%",
-                            ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
-                        })
-                    }
-                });
-
-                TweenMax.to(smallCircles, 0.4, {
-                    scale: "0",
-                    ease: ITEManimate.bezier(0.930, 0.035, 0.350, 0.815),
-
-                });
-
-            }
-
-            //CLOSE
-            function closeAnimation() {
-                TweenMax.to(portfolioContainer, 1.2, {
-                    top: "100%",
-                    ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
-                    width: "100%",
-                    ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
-
-                    onComplete: function() {
-                        TweenMax.to(sceneContainer, 0.8, {
-                            height: "90%",
-                            ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
-                            top: "5%",
-                            ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
-                            width: "90%",
-                            ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
-                            left: "5%",
-                            ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
-                            onComplete: function() {
-                                TweenMax.to(smallCircles, 0.4, {
-                                    scale: "1",
-                                    ease: ITEManimate.bezier(0.815, 0.035, 0.350, 0.930),
-
-                                });
-
-                            }
-
-                        });
-                    }
-
-                })
-
-            }
-
-            animationTrigger.click(function() {
-                if ($(this).attr('data-toggle') == 'closed') {
-                    $(this).attr('data-toggle', 'opened');
-                    openAnimation();
-                } else if ($(this).attr('data-toggle', 'opened')) {
-                    $(this).attr('data-toggle', 'closed');
-                    closeAnimation();
-                }
-            });
-        }
-    };
-
-    $(window).resize(function() {});
-    return main.init();
-}($));
+    const mobile = (e) => {
+        // if change is greater than a quarter of sliderWidth, next else Do NOTHING
+        change > (sliderWidth/4)  ? slide("increase") : null;
+        // if change * -1 is greater than a quarter of sliderWidth, prev else Do NOTHING
+        (change * -1) > (sliderWidth/4) ? slide("decrease") : null;
+        // reset all variable to 0
+        [start, move, change, sliderWidth] = [0,0,0,0]
+    }
+    // call mobile on touch end
+    slider.addEventListener("touchend", mobile)
+})()
